409e0d0a7601b0b7deb56704483cc9d1ea9834af
tristan <tristan.overbay@gmail.com>
2023-06-23 13:33:27 -0500

    small-updates

--- tenmo-server/src/main/java/com/techelevator/tenmo/controller/TransferController.java
+++ tenmo-server/src/main/java/com/techelevator/tenmo/controller/TransferController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 import java.security.Principal;
 import java.util.List;
-
+// comment for push
 @RestController
 @RequestMapping("/dashboard/transfer")
 @PreAuthorize("isAuthenticated()")
@@ -21,14 +21,14 @@ public class TransferController {
     private final TransferDao transferDao;
     private final UserDao userDao;
 
-    public TransferController(JdbcTransferDao transferDao, JdbcUserDao userDao) {
+    public TransferController(TransferDao transferDao, UserDao userDao) {
         this.transferDao = transferDao;
         this.userDao = userDao;
     }
 
     // Handles the POST request to create a new transfer
-    @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public Transfer createTransfer(@RequestBody @Valid Transfer transfer) {
         // Create the transfer using the provided transfer object
         transfer = transferDao.createTransfer(transfer);
@@ -36,8 +36,8 @@ public class TransferController {
     }
 
     // Handles the GET request to retrieve all transfers history for a user
-    @ResponseStatus(HttpStatus.OK)
     @GetMapping(path = "/transfers_history")
+    @ResponseStatus(HttpStatus.OK)
     public List<Transfer> getTransfers(Principal principal) {
         // Retrieve the user's ID based on the principal's name (username)
         int id = userDao.findIdByUsername(principal.getName());
@@ -46,8 +46,8 @@ public class TransferController {
     }
 
     // Handles the GET request to retrieve all pending transfers for a user
-    @ResponseStatus(HttpStatus.OK)
     @GetMapping(path = "/pending_transfers")
+    @ResponseStatus(HttpStatus.OK)
     public List<Transfer> getPendingTransfers(Principal principal) {
         // Retrieve the user's ID based on the principal's name (username)
         int id = userDao.findIdByUsername(principal.getName());
@@ -56,8 +56,8 @@ public class TransferController {
     }
 
     // Handles the GET request to retrieve a transfer by its ID
-    @ResponseStatus(HttpStatus.OK)
     @GetMapping(path = "/{id}")
+    @ResponseStatus(HttpStatus.OK)
     public Transfer getTransferById(@PathVariable int id) {
         // Retrieve the transfer with the provided ID
         return transferDao.getTransfer(id);
@@ -68,9 +68,9 @@ public class TransferController {
     public ResponseEntity<Boolean> updateTransfer(@RequestBody @Valid Transfer transfer) {
         // Update the transfer using the provided transfer object
         if (transferDao.updateTransfer(transfer)) {
-            return new ResponseEntity<>(HttpStatus.OK);
+            return ResponseEntity.ok().build();
         }
         // If the transfer update fails, return an appropriate response with "NOT_FOUND" status
-        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        return ResponseEntity.notFound().build();
     }
 }
